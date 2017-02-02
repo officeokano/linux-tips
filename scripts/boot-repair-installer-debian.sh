2446ff6ab3195ffd51bdc5d5da003e5060ebcd8f
H Okano <h.okano@gmail.com>
2017-02-02 22:30:18 +0900

    moved to scripts

---
+++ scripts/boot-repair-installer-debian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:yannubuntu/boot-repair
+# sudo sed -i 's/wheezy/trusty/g' /etc/apt/sources.list.d/yannubuntu-boot-repair-wheezy.list
+sudo sed -i 's/jessie/trusty/g' /etc/apt/sources.list.d/yannubuntu-boot-repair-jessie.list
+sudo apt-get update;
+sudo apt-get install boot-repair;
