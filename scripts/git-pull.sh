745b8ff8338f02ff6ffffc30b68d093896a7ceeb
okano <okano@bbcoms.co.jp>
2017-05-28 16:25:33 +0900

    modified: scripts/git-pull.sh

--- scripts/git-pull.sh
+++ scripts/git-pull.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-
-files="./*"
+files="*"
 dirary=()
+git=".git"
 for filepath in $files; do
   if [ -d $filepath ] ; then
     dirary+=("$filepath")
   fi
 done
-
 for i in ${dirary[@]}; do
-  cd $i
-  echo updating repository: $i
-  git pull
-  cd ..
+  pushd $i > /dev/null
+  printf "checking directory: $i\n"
+  if [ -e $git ]; then
+    printf "pull from remote\n"
+    git pull
+  else
+    printf "$i is not a git repository\n"
+  fi
+  printf "\n"
+  popd > /dev/null
 done
