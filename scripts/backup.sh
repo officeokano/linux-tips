78c4b5d14fe939d21a62d637c04f802ccabf6869
H Okano <h.okano@gmail.com>
2017-07-08 22:05:10 +0900

    new file:   backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+files="*"
+dirary=()
+for filepath in $files; do
+  if [ -d $filepath ] ; then
+    dirary+=("$filepath")
+  fi
+done
+for i in ${dirary[@]}; do
+  printf "Creating archive: $i...\n"
+  tar czf ../$i.tar.gz --exclude='.*' $i
+done
+printf "Done.\n"
